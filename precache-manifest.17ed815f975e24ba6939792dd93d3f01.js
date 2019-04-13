ca3979d9cd22af061b74e2be33ca897877cc0336
Thomas Gertenbach <Thomas.gertenbach99@gmail.com>
2019-04-13 13:20:08 +0200

    Updates

---
+++ precache-manifest.17ed815f975e24ba6939792dd93d3f01.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2a92be4a160210c89ca3f6f54c44593",
+    "url": "/static/media/logo.a2a92be4.svg"
+  },
+  {
+    "revision": "f666e377f9841827b510bde2cf7dd7e4",
+    "url": "/static/media/Stairsladyfoot.f666e377.svg"
+  },
+  {
+    "revision": "69eeed4d3b6eebc56ff07e23020859ff",
+    "url": "/static/media/Baloo-Regular.69eeed4d.ttf"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "61dc78cf7d44578d97d6",
+    "url": "/static/js/main.9603584e.chunk.js"
+  },
+  {
+    "revision": "2e80777aa8eac2d29b43",
+    "url": "/static/js/2.93bb1485.chunk.js"
+  },
+  {
+    "revision": "61dc78cf7d44578d97d6",
+    "url": "/static/css/main.dd30965c.chunk.css"
+  },
+  {
+    "revision": "4d8e42b0819c81fa358193b666e2bdd2",
+    "url": "/index.html"
+  }
+];
